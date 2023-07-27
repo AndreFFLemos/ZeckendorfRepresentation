4d5ee7e628e828e3797cb298c4579c69d97a1672
andrefflemos <afontelonga@gmail.com>
2023-07-27 18:56:23 +0100

    tried using bigint but outofmemory exception still happens

--- Zeckendorfs/src/main/java/org/example/Generate.java
+++ Zeckendorfs/src/main/java/org/example/Generate.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,26 +8,26 @@ import java.util.Random;
 public class Generate {
 
 
-    public List<Long> findFibonnaciNumbers (long a, long b){
+    public List<BigInteger> findFibonnaciNumbers (BigInteger a, BigInteger b){
 
-        List<Long> fibonacciNumbers= new ArrayList<>();
+        List<BigInteger> fibonacciNumbers= new ArrayList<>();
         //The first two int in a Fibonacci list are
-        long lowerNumber = 0;
-        long higherNumber = 1;
+        BigInteger lowerNumber = BigInteger.ZERO;
+        BigInteger higherNumber = BigInteger.ONE;
 
         //so if a is 0, then add it
-        if(a == 0) {
-            fibonacciNumbers.add(0L);
+        if(a.equals(BigInteger.ZERO)){
+            fibonacciNumbers.add(a);
         }
 
         //let's enter the loop and run it until higherNumber reaches the value inside b
-        while(higherNumber <= b){
+        while(higherNumber.compareTo(b)<=0) {
             //If the fibonacci number is between a and b add it to the list
-            if(higherNumber >= a){
+            if(higherNumber.compareTo(a)>=0){
                 fibonacciNumbers.add(higherNumber);
             }
             //since a Fibonnaci number is the result of the sum of the two previous Fibonacci numbers
-            long sum = lowerNumber + higherNumber;
+            BigInteger sum = lowerNumber.add(higherNumber);
             //then the lower Fibonacci Number receives the higher Fibonnaci Number
             lowerNumber = higherNumber;
             //and the higher number receives the sum value because it is the next Fibonnaci number
@@ -37,26 +38,26 @@ public class Generate {
     }
 
     //this method finds the list of non-sequential Fibonacci's numbers which their sum is a particular number
-    public List<Long> zeckendorfsRepresentation (long number, List<Long> fibonacciNumbers) {
+    public List<BigInteger> zeckendorfsRepresentation (BigInteger number, List<BigInteger> fibonacciNumbers) {
 
-        List<Long> fibonnacisInTheRepresentation = new ArrayList<>();
+        List<BigInteger> fibonnacisInTheRepresentation = new ArrayList<>();
 
         //if the number is 0, add it to the Representation and return
-        if (number== 0) {
-            fibonnacisInTheRepresentation.add(0L);
+        if (number.equals(BigInteger.ZERO)) {
+            fibonnacisInTheRepresentation.add(BigInteger.ZERO);
             return fibonnacisInTheRepresentation;
         }
 
         //now to check if the numbers aren't sequential we have to run through each in the fibonacci's list
         for (int i = fibonacciNumbers.size()-1; i > 0; i--) {
             //the last added Fibonacci number is the highest one so that is the first to be used
-            long currentFibonacciNumber = fibonacciNumbers.get(i);
+            BigInteger currentFibonacciNumber = fibonacciNumbers.get(i);
             //then we compare the Fibonacci's number in the current iteration to an integer,
             // if it's smaller add it in the representation and we update the integer value to find the next.
             // So if the Number is 71, then 71-55=16. In the second iteration we compare 34<16, in the third 21<16, in the 4th 13<16...
-            if (currentFibonacciNumber <= number) {
+            if (currentFibonacciNumber.compareTo(number)<=0) {
                 fibonnacisInTheRepresentation.add(currentFibonacciNumber);
-                number = number - currentFibonacciNumber;
+                number = number.subtract(currentFibonacciNumber);
             }
         }
 
@@ -64,14 +65,14 @@ public class Generate {
 
     }
     //this method converts the Zeckendorf's representation to a StringBuilder in order to show each number in the sum
-    public StringBuilder convertToStringBuilder (List <Long> fibonnacisInTheRepresentation ){
+    public StringBuilder convertToStringBuilder (List <BigInteger> fibonnacisInTheRepresentation ){
 
         StringBuilder fibonnacisInTheRepresentationAsAString= new StringBuilder();
 
         //run through each number in the list and add it to the StringBuilder instance and after add a + sign
         if (!fibonnacisInTheRepresentation.isEmpty()) {
-            for (int i = 0; i < fibonnacisInTheRepresentation.size(); i++) {
-                fibonnacisInTheRepresentationAsAString.append(fibonnacisInTheRepresentation.get(i));
+            for (BigInteger fibonacciNumber: fibonnacisInTheRepresentation){
+                fibonnacisInTheRepresentationAsAString.append(fibonacciNumber.toString());
                 fibonnacisInTheRepresentationAsAString.append("+");
             }
             //because the last char added is a +, we have to remove it.
@@ -81,12 +82,12 @@ public class Generate {
     }
 
     //this method gets the Zeckendorf sum for all numbers in a range
-    public void getTheZeckendorfSumForAllNumbers(Long lowerRange, Long upperRange ){
+    public void getTheZeckendorfSumForAllNumbers(BigInteger lowerRange, BigInteger upperRange ){
 
-        List<Long> fibonacciNumbers= findFibonnaciNumbers(0,upperRange);
+        List<BigInteger> fibonacciNumbers= findFibonnaciNumbers(BigInteger.ZERO,upperRange);
 
-        for (long i= lowerRange; i<=upperRange;i++){
-            List<Long> zeckendorfRepresentation=zeckendorfsRepresentation(i,fibonacciNumbers);
+        for (BigInteger i= lowerRange; i.compareTo(upperRange) <= 0; i = i.add(BigInteger.ONE)){
+            List<BigInteger> zeckendorfRepresentation=zeckendorfsRepresentation(i,fibonacciNumbers);
             System.out.println(convertToStringBuilder(zeckendorfRepresentation));
         }
     }
--- Zeckendorfs/src/main/java/org/example/main.java
+++ Zeckendorfs/src/main/java/org/example/main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class main {
 
@@ -9,12 +12,11 @@ public class main {
         Generate generate= new Generate();
 
         Random random= new Random();
-        long a= random.nextLong(0L,2147483647L);
-        long b= random.nextLong(a,2147483647L);
+        BigInteger a = BigInteger.valueOf(random.nextInt(2147483647));
+        BigInteger b = new BigInteger(2147483647 - a.intValue(), random).add(a);
 
-        System.out.println("A is " + a);
-        System.out.println("B is " + b);
-        generate.getTheZeckendorfSumForAllNumbers(0L,71L);
+        generate.getTheZeckendorfSumForAllNumbers(a,b);
+
     }
 
 }
