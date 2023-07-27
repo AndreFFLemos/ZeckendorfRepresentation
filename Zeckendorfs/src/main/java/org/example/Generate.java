c02f75c5ff3b1ce8af2068dcfcd4617e6e7cd51e
andrefflemos <afontelonga@gmail.com>
2023-07-27 12:03:33 +0100

    method built first iteration

--- Zeckendorfs/src/main/java/org/example/Generate.java
+++ Zeckendorfs/src/main/java/org/example/Generate.java
@@ -36,17 +36,25 @@ public class Generate {
         return fibonacciNumbers;
     }
 
-
-    //now that i have the list of fibonacci numbers i can check what Fib. numbers are in the sum of the representation
     public String zeckendorfsRepresentation (long number){
 
         List <Long> fibonaccis= new ArrayList<>();
         fibonaccis= findFibonnaciNumbers(0,number);
 
+        List<Long> fibonnacisInTheRepresentation= new ArrayList<>();
 
+        if (fibonaccis.get(fibonaccis.size()-1)==0){
+            return number + "=" +fibonnacisInTheRepresentation.get(0);
+        }
 
+        for (int i=fibonaccis.size()-1;i>=0;i++){
+            long currentFibonacciNumber= fibonaccis.get(i);
+            if (currentFibonacciNumber<=number) {
+                fibonnacisInTheRepresentation.add(currentFibonacciNumber);
+                number = number - currentFibonacciNumber;
+            }
+        }
 
-
-        
+        ;
     }
 }
